cea4b9da541945ea455d961a684ff6f2011e3e8b
sanjeev-rm <sanjeevraghu2050@gmail.com>
2023-06-12 02:12:25 +0530

    Initial Commit

---
+++ Orom Cycles/Orom_CyclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orom_CyclesApp.swift
+//  Orom Cycles
+//
+//  Created by Sanjeev RM on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Orom_CyclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
